b980765c1a6c3a7d02d402e27b722015ad26f857
Arthur Jackson <retroplayz919@gmail.com>
2023-03-12 20:52:36 -0400

    created algorithms

---
+++ test.py
@@ -0,0 +1,17 @@
+# Variables
+x = 4
+y = 7
+z = x + y 
+print(z)
+a = y - x 
+print(a)
+# flow control
+# data types
+# functions
+# alogorithms
+# object oriented programming
+# debugging
+# how to write and compile
+# calculate the area of rectangle
+# calculate the circumference of a circle
+# calculate the speed vs distance problem we discussed
